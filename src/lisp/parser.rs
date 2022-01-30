74206f59fd7f1627fc4bbf4d5b882d7871ade07e
Alex Ambarchian <alex@alexambarchian.com>
2022-01-29 22:38:43 -0500

    Parse negative numbers

--- src/lisp/parser.rs
+++ src/lisp/parser.rs
@@ -1,12 +1,12 @@
 use crate::lib::string_parser::parse_string;
 use crate::util::Literal;
 use nom::{
-    branch::alt, character::complete::digit1, combinator::recognize, sequence::delimited, IResult,
+    branch::alt, character::complete::{char, digit1}, sequence::preceded, IResult, multi::many0,
 };
 
 /// Parse a (possibly negative) number
 fn parse_number(input: &str) -> IResult<&str, Literal> {
-    let (input, num_text) = digit1(input)?;
+    let (input, num_text) = preceded(many0(char('-')), digit1)(input)?;
     Ok((input, Literal::Number(num_text.parse::<i64>().unwrap())))
 }
 
