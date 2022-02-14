85c645f0078207f37a4b109ce5e37f0f2c010808
Alex Ambarchian <alex@alexambarchian.com>
2022-02-14 10:00:02 -0500

    Add Scope struct

--- src/lisp/interpreter.rs
+++ src/lisp/interpreter.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use super::parser::parse_file;
 use crate::lisp::LanguageType;
 use anyhow::Result;
@@ -5,4 +6,11 @@ use anyhow::Result;
 pub fn interpret(file_contents: &str) -> Result<(&str, Vec<LanguageType>)> {
     let (input, result) = parse_file(file_contents).unwrap();
     Ok((input, result))
+}
+
+struct Scope<'a> {
+    name: String,
+    locals: HashMap<String, LanguageType>,
+    parent: &'a Scope<'a>,
+    children: Vec<&'a Scope<'a>>,
 }
